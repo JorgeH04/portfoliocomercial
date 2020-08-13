f928c892c49e26f4d150d9ba6b94f5eb6d0f8938
Jorge <jhessle04@gmail.com>
2020-08-13 20:54:49 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-} 
+//if (process.env.NODE_ENV !== 'production') {
+ // require('dotenv').config();
+//} 
 
 
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const methodOverride = require('method-override');
-const passport = require('passport');
+//const methodOverride = require('method-override');
+//const passport = require('passport');
 //const mongoose = require('mongoose');
 
 // Initializations
